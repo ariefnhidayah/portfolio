9b22baff04adaaceae6fb7585c6175b220942924
Arief <ariefnhidayah@gmail.com>
2021-09-07 21:33:02 +0700

    update

--- js/main.js
+++ js/main.js
@@ -227,7 +227,7 @@
 			$.ajax({      	
 
 		      type: "POST",
-		      url: "inc/sendEmail.php",
+		      url: "https://backend-ariefnhidayah.000webhostapp.com/inc/sendEmail.php",
 		      data: $(form).serialize(),
 		      beforeSend: function() { 
 
